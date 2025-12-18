653f86357a56a8fa7f2470474ed6795d71298b85
bala-amypo <bala@amypo.in>
2025-12-18 08:58:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AnomalyRule.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+public class AnomalyRule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true)
+    private String ruleCode;
+    private String description;
+    private String thresholdType;
+    private Double thresholdValue;
+    private Boolean active;
+    public AnomalyRule() {
+    }
+    public AnomalyRule(Long id, String ruleCode, String description, String thresholdType, Double thresholdValue,
+            Boolean active) {
+        this.id = id;
+        this.ruleCode = ruleCode;
+        this.description = description;
+        this.thresholdType = thresholdType;
+        this.thresholdValue = thresholdValue;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRuleCode() {
+        return ruleCode;
+    }
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getThresholdType() {
+        return thresholdType;
+    }
+    public void setThresholdType(String thresholdType) {
+        this.thresholdType = thresholdType;
+    }
+    public Double getThresholdValue() {
+        return thresholdValue;
+    }
+    public void setThresholdValue(Double thresholdValue) {
+        this.thresholdValue = thresholdValue;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    
+
+
+
+}
