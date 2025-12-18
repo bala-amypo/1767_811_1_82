a972ccd01f38effcfbdde517e8b8f3b5394415ec
bala-amypo <bala@amypo.in>
2025-12-18 16:12:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String passwordHash;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> role;
+
+    private LocalDateTime createdAt;
+
+    public UserAccount() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public UserAccount(Long id, String username, String email,
+                       String passwordHash, Set<String> role,
+                       LocalDateTime createdAt) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public Set<String> getRole() { return role; }
+    public void setRole(Set<String> role) { this.role = role; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
