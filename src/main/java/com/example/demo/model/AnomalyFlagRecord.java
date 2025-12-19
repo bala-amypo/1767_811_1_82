ed69846eb62c13649b5369fd08ee4dfa0e005b03
bala-amypo <bala@amypo.in>
2025-12-19 10:08:22 +0000

    User commit

--- src/main/java/com/example/demo/model/AnomalyFlagRecord.java
+++ src/main/java/com/example/demo/model/AnomalyFlagRecord.java
@@ -1,94 +1,88 @@
 package com.example.demo.model;
 
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+
 
 @Entity
 public class AnomalyFlagRecord {
+@Id
+@GeneratedValue(strategy =GenerationType.IDENTITY)
+private Long id;
+private Long employeeid;
+private Long metricid;
+private String ruleCode;
+private String severity;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+private String details;
+private LocalDateTime flaggedAt=LocalDateTime.now();
+private Boolean resolved =false;
+public AnomalyFlagRecord() {
+}
 
-    private Long employeeId;
-    private Long metricId;
-    private String ruleCode;
-    private String severity;
-    private String details;
-    private LocalDateTime flaggedAt;
-    private Boolean resolved = false;
+public AnomalyFlagRecord(Long id, Long employeeid, Long metricid, String ruleCode, String severity, String details,
+        LocalDateTime flaggedAt, Boolean resolved) {
+    this.id = id;
+    this.employeeid = employeeid;
+    this.metricid = metricid;
+    this.ruleCode = ruleCode;
+    this.severity = severity;
+    this.details = details;
+    this.flaggedAt = flaggedAt;
+    this.resolved = resolved;
+}
 
-    public AnomalyFlagRecord() {
-    }
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public Long getEmployeeid() {
+    return employeeid;
+}
+public void setEmployeeid(Long employeeid) {
+    this.employeeid = employeeid;
+}
+public Long getMetricid() {
+    return metricid;
+}
+public void setMetricid(Long metricid) {
+    this.metricid = metricid;
+}
+public String getRuleCode() {
+    return ruleCode;
+}
+public void setRuleCode(String ruleCode) {
+    this.ruleCode = ruleCode;
+}
+public String getSeverity() {
+    return severity;
+}
+public void setSeverity(String severity) {
+    this.severity = severity;
+}
+public String getDetails() {
+    return details;
+}
+public void setDetails(String details) {
+    this.details = details;
+}
+public LocalDateTime getFlaggedAt() {
+    return flaggedAt;
+}
+public void setFlaggedAt(LocalDateTime flaggedAt) {
+    this.flaggedAt = flaggedAt;
+}
+public Boolean getResolved() {
+    return resolved;
+}
+public void setResolved(Boolean resolved) {
+    this.resolved = resolved;
+}
 
-    @PrePersist
-    protected void onCreate() {
-        this.flaggedAt = LocalDateTime.now();
-    }
 
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public Long getMetricId() {
-        return metricId;
-    }
-
-    public void setMetricId(Long metricId) {
-        this.metricId = metricId;
-    }
-
-    public String getRuleCode() {
-        return ruleCode;
-    }
-
-    public void setRuleCode(String ruleCode) {
-        this.ruleCode = ruleCode;
-    }
-
-    public String getSeverity() {
-        return severity;
-    }
-
-    public void setSeverity(String severity) {
-        this.severity = severity;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public LocalDateTime getFlaggedAt() {
-        return flaggedAt;
-    }
-    
-
-    public Boolean getResolved() {
-        return resolved;
-    }
-
-    public void setResolved(Boolean resolved) {
-        this.resolved = resolved;
-    }
 }
