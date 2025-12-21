8c3921e6d7739bf759e4e920306384f70a367779
bala-amypo <bala@amypo.in>
2025-12-21 14:19:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    Optional<UserAccount> findByUsernameAndPassword(String username, String password);
+    Optional<UserAccount> findByUsernameAndPasswordHash(String username, String passwordHash);
 }
