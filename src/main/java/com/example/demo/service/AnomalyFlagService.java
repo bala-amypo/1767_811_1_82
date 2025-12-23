8933f32b11cd70a149108dc0e806381294a72d98
bala-amypo <bala@amypo.in>
2025-12-23 08:26:58 +0000

    User commit

--- src/main/java/com/example/demo/service/AnomalyFlagService.java
+++ src/main/java/com/example/demo/service/AnomalyFlagService.java
@@ -5,6 +5,7 @@ import com.example.demo.model.AnomalyFlagRecord;
 import java.util.List;
 
 public interface AnomalyFlagService {
+    
 
     AnomalyFlagRecord flagAnomaly(AnomalyFlagRecord anomalyFlagRecord);
 
