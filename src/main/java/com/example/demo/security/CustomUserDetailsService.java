ff3d28d9d404d47b4a3d4c22c97585e2bef904b9
bala-amypo <bala@amypo.in>
2025-12-25 14:47:06 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,31 +1,4 @@
-package com.example.demo.controller;
-
-import com.example.demo.security.JwtTokenProvider;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
-
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
-
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtProvider;
-
-    public AuthController(AuthenticationManager authenticationManager,
-                          JwtTokenProvider jwtProvider) {
-        this.authenticationManager = authenticationManager;
-        this.jwtProvider = jwtProvider;
-    }
-
-    @PostMapping("/login")
-    public Map<String, String> login(@RequestBody LoginRequest request) {
-
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUsername(),package com.example.demo.security;
+package com.example.demo.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,15 +11,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
-
         throw new UsernameNotFoundException("User not found");
-    }
-}
-
-                        request.getPassword())
-        );
-
-        String token = jwtProvider.generateToken(request.getUsername());
-        return Map.of("token", token);
     }
 }
