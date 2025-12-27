6f4c0b318b05a9f324b7631c22adbec225dacbce
bala-amypo <bala@amypo.in>
2025-12-27 03:49:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,28 +1,28 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.model.UserAccount;
-import com.example.demo.service.UserAccountService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.model.UserAccount;
+// import com.example.demo.service.UserAccountService;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-@Tag(name = "Authentication")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// @Tag(name = "Authentication")
+// public class AuthController {
 
-    private final UserAccountService service;
+//     private final UserAccountService service;
 
-    public AuthController(UserAccountService service) {
-        this.service = service;
-    }
+//     public AuthController(UserAccountService service) {
+//         this.service = service;
+//     }
 
-    @PostMapping("/register")
-    public UserAccount register(@RequestBody UserAccount user) {
-        return service.registerUser(user);
-    }
+//     @PostMapping("/register")
+//     public UserAccount register(@RequestBody UserAccount user) {
+//         return service.registerUser(user);
+//     }
 
-    @PostMapping("/login")
-    public String login() {
-        return "Login successful";
-    }
-}
+//     @PostMapping("/login")
+//     public String login() {
+//         return "Login successful";
+//     }
+// }
